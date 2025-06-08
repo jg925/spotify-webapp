32713a672c857180f4165f5ebd08c4c76135bed4
jg925 <jg925@cornell.edu>
2025-06-08 00:26:36 -0700

    add live playlist count and rename title

--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,12 @@ export default async function Page() {
   return (
     <div className="pageContainer">
       <div className="text">
-        <h1>Shadow's Public Spotify Playlists</h1>
+        <h1>My Public Spotify Playlists</h1>
         <p>Click and hold the Shift key to drag the playlists around.</p>
-        <p>I have **too many** playlists, they take a while to load.</p>
+        <p>
+          I have {playlistIds.length} public playlists, they take a while to
+          load.
+        </p>
       </div>
 
       <PlaylistStack playlistIds={playlistIds} />
